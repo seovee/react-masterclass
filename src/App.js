acc47e608e6967559f8cbf5e4cbacf6e880e49b9
seovee <jinseob102@naver.com>
2023-05-30 17:10:21 +0900

    2.4 ~ 2.5 animation(keyframes)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const rotationAnimation = keyframes`
+0% {
+  transform: rotate(0deg);
+  border-radius:20px;
+}
+50% {
+  transform: rotate(180deg);
+  border-radius:100px;
+}
+0% {
+  transform: rotate(360deg);
+  border-radius:20px;
+}
+`;
+
+const Emoji = styled.span`
+  font-size: 70px;
+  transition: all 1s;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${rotationAnimation} 3s linear infinite;
+  transition: all 1s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji}:hover {
+    font-size: 130px;
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>🔥</Emoji>
+    </Wrapper>
   );
 }
 
