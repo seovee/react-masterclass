467914466e66221beae7ecf6f26dc7bb8f8a8c2a
seovee <jinseob102@naver.com>
2023-05-30 23:12:33 +0900

    2.6 ~ 2.7 복습 및 Themes 사용하기

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-0% {
-  transform: rotate(0deg);
-  border-radius:20px;
-}
-50% {
-  transform: rotate(180deg);
-  border-radius:100px;
-}
-0% {
-  transform: rotate(360deg);
-  border-radius:20px;
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 70px;
-  transition: all 1s;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 3s linear infinite;
-  transition: all 1s;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  ${Emoji}:hover {
-    font-size: 130px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
