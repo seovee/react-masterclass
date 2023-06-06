a48fd1e1988b6abcc9b54e0ad2ff51916002bd83
seovee <jinseob102@naver.com>
2023-06-06 17:09:13 +0900

    비밀번호(credential)오류 확인용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <Circle bgColor="Tomato" />
-      <Circle bgColor="teal" text="김진섭이다아아아아아아아" />
+      <Circle bgColor="teal" text="크레덴셜 오류 수정해봤습니다." />
     </div>
   );
 }
