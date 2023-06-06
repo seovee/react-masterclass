bac13fb98059c6d251fa7ebb6d6dbc18ca1df2e8
seovee <jinseob102@naver.com>
2023-06-06 17:03:59 +0900

    ~ 3.3 Optional Props까지

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="Tomato" />
+      <Circle bgColor="teal" text="김진섭이다아아아아아아아" />
+    </div>
+  );
+}
+
+export default App;
