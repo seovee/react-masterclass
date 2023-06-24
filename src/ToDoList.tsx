537f2a492c78c23b08d6bb8df6d6f5d9be81b15e
seovee <jinseob102@naver.com>
2023-06-24 18:08:51 +0900

    6.12 리코일 useRecoilState로 리팩

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,17 +1,33 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+  id: number;
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
 function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const onSubmit = (data: IForm) => {
-    console.log("add to do", data.toDo);
-    setValue("toDo", "전송했습니다");
+  const onSubmit = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category: "TO_DO" }, ...oldToDos]);
+    setValue("toDo", "");
   };
   return (
     <div>
+      <h1>To Dos</h1>
+      <hr />
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register("toDo", { required: "할 일을 적어주세요" })}
@@ -19,6 +35,11 @@ function ToDoList() {
         />
         <button>Add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
