8a6418693ad775b3aeb525676ba1b1aea94c1544
seovee <jinseob102@naver.com>
2023-06-27 16:55:46 +0900

    6.13 ~ 14. 완료

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,38 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    });
+  };
   return (
     <li>
-      {text}
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
