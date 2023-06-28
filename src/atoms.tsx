fe128c6d4f2a181861dc0dd24bbea1018235fba5
seovee <jinseob102@naver.com>
2023-06-28 16:37:32 +0900

    7.0 시작(페이지 수정 많음)

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,28 +1,14 @@
 import { atom, selector } from "recoil";
 
-export interface IToDo {
-  text: string;
-  category: "TO_DO" | "DOING" | "DONE";
-  id: number;
-}
-
-export const categoryState = atom({
-  key: "category",
-  default: "TO_DO",
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    if (category === "TO_DO") return toDos.filter((toDo) => toDo.category === "TO_DO");
-    if (category === "DOING") return toDos.filter((toDo) => toDo.category === "DOING");
-    if (category === "DONE") return toDos.filter((toDo) => toDo.category === "DONE");
+    const minutes = get(minuteState);
+    return minutes / 60;
   },
 });
