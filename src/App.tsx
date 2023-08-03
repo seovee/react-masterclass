101de2469bdbc9e2c0a4cfb706b5afe4dd464727
seovee <jinseob102@naver.com>
2023-08-03 22:21:05 +0900

    8 완료

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,33 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 `;
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
+const Board = styled.div`
+  display: flex;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
   height: 200px;
+  width: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   font-size: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  background-color: orange;
+  height: 80px;
+  width: 80px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -39,27 +46,39 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 
+const Switch = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 18px;
+  font-weight: 500;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
   const [id, setId] = useState<null | string>(null);
+  const [clicked, setClicked] = useState("2");
+  const toggleSwitch = () => {
+    if (clicked === "2") {
+      return setClicked((prev) => "3");
+    }
+    if (clicked === "3") {
+      return setClicked((prev) => "2");
+    }
+  };
   return (
     <Wrapper>
-      <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+      <Board>
+        {["1", "2", "3", "4"].map((i) => (
+          <Box key={i}>
+            <AnimatePresence>
+              {i === clicked ? <Circle layoutId="circle" /> : null}
+            </AnimatePresence>
+          </Box>
         ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
-          >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
+      </Board>
+      <Switch onClick={toggleSwitch}>Switch</Switch>
     </Wrapper>
   );
 }
